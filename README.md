cc199dfd47fb612e89e838bec7c00925bec20c45
Sahan Kumarasinghe <sahansk2@illinois.edu>
2021-03-20 02:56:15 -0400

    License SecretLatexBot as AGPLv3

---
+++ README.md
@@ -0,0 +1,25 @@
+# SecretLatexBot
+
+A Discord bot that detects LaTeX between `$||` and `||` and sends the resulting LaTeX in a spoiler image. For example, in `Here is $||3||`, 3 will be detected and sent in a spoiler image.
+
+This bot currently uses the Google Charts API on the backend.
+
+This bot is licensed under AGPLv3 - what this means is that (among other things), you can modify, redistribute, and use this bot however you want, as long as you make the source available to whoever is using the bot, and as long as you retain copyright license notices in the code. A convenient way of doing this is to change the `SRC_LOC` variable in `app.ts` accordingly.
+
+
+## License
+
+Copyright (C) 2021 Sahan Kumarasinghe
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,22 @@
+/*
+  Copyright (2021) Sahan Kumarasinghe.
+
+  This file is part of SecretLatexBot.
+
+  SecretLatexBot is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as
+  published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
+
+  SecretLatexBot is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import { Client, Message, MessageAttachment, MessageEmbed } from 'discord.js';
 import { findExpressions, exprToURL } from './match';
 
@@ -5,6 +24,7 @@ require('dotenv').config()
 
 const client: Client = new Client()
 const BOT_KEY = process.env.BOT_KEY;
+const SRC_LOC: string = "https://github.com/sahansk2/secretlatexbot";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user?.tag}!`);
@@ -17,7 +37,9 @@ client.on('message', (msg) => {
   const delim: number = content.indexOf("$||") 
   if (delim !== -1) {
     if (content.trim() == "$||help") {
-      msg.channel.send(`Send Latex expressions in spoiler tags! Just wrap your latex expression like this: \`$|| YOUR EXPRESSION HERE ||\`. Supports multiple expressions in the same message. For example: Three divided by four is $||\\frac{3}{4}||, as opposed to $||\\frac{4}{5}||.`)
+      msg.channel.send(`Send Latex expressions in spoiler tags! Just wrap your latex expression like this: \`$|| YOUR EXPRESSION HERE ||\`. Supports multiple expressions in the same message. For example: Three divided by four is $||\\frac{3}{4}||, as opposed to $||\\frac{4}{5}||. Type \`$||source\` for access to source code.`)
+    } else if (content.trim() == "$||source") {
+      msg.channel.send(`The source code for this bot is available at ${SRC_LOC}.`)
     } else {
       const expressions = findExpressions(content)
       if (expressions.length > 0)
