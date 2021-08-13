212db5e7da3a0155db0dc251bb3f85f1bcf5c32c
Sahan Kumarasinghe <sahansk2@illinois.edu>
2021-08-13 13:41:55 -0400

    Delete literally all the code for the bot, we're doing this again

--- src/bot/app.ts
+++ src/bot/app.ts
@@ -18,64 +18,16 @@
 */
 
 import { Client, Message } from 'discord.js';
-import msg_db from './db/db'
-import { detect_content, clear_linked_messages } from './eventhooks'
-
-const client: Client = new Client({ partials: ['MESSAGE', 'REACTION'] })
 
 const BOT_KEY = process.env.BOT_KEY;
 
 process.on('SIGINT', () => {
   console.log('Bye for now!')
-  msg_db.close()
   process.exit()
 })
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user?.tag}!`);
-  client.user?.setPresence({ activity: { name: '$||help' }})
-  console.log('Set presence.')
-});
+function runClient() {
 
-client.on('message', (msg) => {
-  console.log('detected new message', msg.id)
-  detect_content(msg)
-});
-
-client.on('messageDelete', (msg) => {
-  console.log('detected messageDelete on', msg.id)
-  // If it's partial, we don't care - we can't do anything about it, because we only have the msg id and nothing else.
-  if (!msg.partial && msg.author.id != client.user!.id) {
-    clear_linked_messages(msg as Message)
-  }
-})
-
-client.on('messageUpdate', (old_msg, new_msg) => {
-  console.log('detected messageUpdate from', old_msg.id, 'to', new_msg.id)
-  // If it's partial, then that means it's probably just uncached. 
-  let message_promises: Array<Promise<Message>> = []
-  if (old_msg.partial) {
-    message_promises.push(old_msg.fetch())
-  } else {
-    message_promises.push(new Promise(resolve => resolve(old_msg as Message)))
-  }
-
-  if (new_msg.partial) {
-    message_promises.push(new_msg.fetch())
-  } else {
-    message_promises.push(new Promise(resolve => resolve(new_msg as Message)))
-  }
-
-  Promise.all(message_promises)
-    .then(([old_msg, new_msg]) => {
-      if (old_msg.content == new_msg.content) return;
-      clear_linked_messages(old_msg)
-      detect_content(new_msg)
-  }).catch((err) => console.log(err))
-})
-
-const clientInit = () => {
-  client.login(process.env.BOTKEY)
 }
 
-export default clientInit
+export default runClient
